c1fb73c3f60506cbe380f9be274c12060d5ec9ad
EmPuiquin24 <hepuiquin@proton.me>
2026-04-24 20:33:08 -0500

    feat: define los tipos de datos para el programa

---
+++ schemas.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, EmailStr
+from typing import List, Optional
+from datetime import date # IMPORTANTE: Para validar AAAA-MM-DD
+from models import SexoEnum
+
+
+# --- ESQUEMAS DE MASCOTAS ---
+class MascotaBase(BaseModel):
+    nombre: str
+    especie: str
+    raza: Optional[str] = None
+    sexo: SexoEnum
+    fecha_nacimiento: date
+    peso: float 
+    esterilizado: bool
+    observaciones_generales: Optional[str] = None
+
+class MascotaCreate(MascotaBase):
+    id_dueno: int 
+
+class Mascota(MascotaBase):
+    id: int
+
+    class Config:
+        from_attributes = True 
+
+# --- ESQUEMAS DE DUEÑOS ---
+class DuenoBase(BaseModel):
+    nombre: str
+    telefono: str
+    correo: EmailStr
+
+class DuenoCreate(DuenoBase):
+    pass
+
+class Dueno(DuenoBase):
+    id: int
+    mascotas: List[Mascota] = []
+
+    class Config:
+        from_attributes = True
