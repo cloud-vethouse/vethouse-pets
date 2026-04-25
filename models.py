3ea4895123a3009e898b4f716959548c016b5584
EmPuiquin24 <hepuiquin@proton.me>
2026-04-24 20:32:47 -0500

    feat: define los models para las entidades en la base de datos

---
+++ models.py
@@ -0,0 +1,32 @@
+import enum
+from sqlalchemy import Column, Integer, Float, String, Date, Boolean, ForeignKey, Enum as SqlEnum
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class SexoEnum(str, enum.Enum):
+    MACHO = "Macho"
+    HEMBRA = "Hembra"
+
+class Dueno(Base):
+    __tablename__ = "duenos"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(100))
+    telefono = Column(String(20))
+    correo = Column(String(100), unique=True)
+    mascotas = relationship("Mascota", back_populates="propietario")
+
+class Mascota(Base):
+    __tablename__ = "mascotas"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(100))
+    especie = Column(String(50))
+    raza = Column(String(50))
+    sexo = Column(SqlEnum(SexoEnum), nullable=False)
+    fecha_nacimiento = Column(Date)
+    peso = Column(Float)
+    esterilizado = Column(Boolean, default=False)
+    observaciones_generales = Column(String(250))
+    id_dueno = Column(Integer, ForeignKey("duenos.id"))
+    
+    propietario = relationship("Dueno", back_populates="mascotas")
